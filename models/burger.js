ce14a342013d2bc0f250b9fc325b9aadd7f239de
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-07-30 09:33:51 -0400

    added model for burger

---
+++ models/burger.js
@@ -0,0 +1,38 @@
+const orm = require('../config/orm.js');
+
+const burger = {
+    /**
+     * sends the table name and callback function to the orm connection to the database; all records are returned to the callback function
+     * 
+     * @param {function} callback 
+     */
+    all: function(callback) {
+        orm.selectAll('burgers', (results) => {
+            callback(results);
+        });
+    },
+    /**
+     * sends the table name and new record definition (key/value pairs) to the orm connection to the database; status returned to the callback function
+     * @param {object} pairs 
+     * @param {function} callback 
+     */
+    create: function(pairs, callback) {
+        orm.insertOne('burgers', pairs, (results) =>{
+            callback(results);
+        });
+    },
+    /**
+     * sends the table name and columns to update for the record that matches id to the orm connection to the database; status returned to the callback function
+     * 
+     * @param {object} pairs 
+     * @param {string} id 
+     * @param {function} callback 
+     */
+    udpate: function(pairs, id, callback) {
+        orm.updateOne('burgers', pairs, id, (results) =>{
+            callback(results);
+        });
+    }
+}
+
+module.exports = burger;
