ae4702f777c0ea9750c15f5bb2b8bda2123bc198
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-08-01 15:52:29 -0400

    updated connection for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 require('dotenv').config(); // guard private login data
 const mysql = require('mysql');
 
-if (process.env.JAWSDB_URL) {
-    const connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    const connection = mysql.createConnection({
-        host: process.env.HOST,
-        port: process.env.DBHOST,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE
-    });
+const connection = () => {
+    if (process.env.JAWSDB_URL) {
+        mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+        mysql.createConnection({
+            host: process.env.HOST,
+            port: process.env.DBHOST,
+            user: process.env.USER,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE
+        });
+    }
 }
 
 module.exports = connection;
